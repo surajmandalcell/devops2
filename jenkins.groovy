156bbdc3a60dbb879dd2717119511ec9b6903ce9
Suraj Mandal <surajmandalcell@gmail.com>
2023-08-07 02:31:33 -0400

    Final build fix complete

--- jenkins.groovy
+++ jenkins.groovy
@@ -2,35 +2,18 @@ pipeline {
     agent any
 
     stages {
-        stage('Clone Repository') {
+        stage('Get Code') {
             steps {
                 git branch: 'master', url: 'https://github.com/surajmandalcell/devops2.git'
             }
         }
 
-        stage('Build Docker Image') {
+        stage('Build & Push') {
             steps {
                 script {
                     def dockerImage = docker.build("cruelplatypus67/devops2:${env.BUILD_ID}", "-f Dockerfile .")
                     env.IMAGE_NAME = "cruelplatypus67/devops2:${env.BUILD_ID}"
-                }
-            }
-        }
-
-        stage('Test Docker Image') {
-            steps {
-                script {
-                    sh "docker run --rm ${env.IMAGE_NAME}"
-                }
-            }
-        }
-
-        stage('Push Docker Image') {
-            steps {
-                script {
-                    withDockerRegistry([url: "https://index.docker.io/v1/", credentialsId: "dockerhub_credential"]) {
-                        sh "docker push ${env.IMAGE_NAME}"
-                    }
+                    sh "docker image push cruelplatypus67/devops2:${env.BUILD_ID}"
                 }
             }
         }
