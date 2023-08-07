c7fcbc3e27112c1a9621ac35bc368813c0bcce83
Suraj Mandal <surajmandalcell@gmail.com>
2023-08-07 03:36:07 -0400

    - Update groovy - Start adding helm

--- jenkins.groovy
+++ jenkins.groovy
@@ -14,6 +14,8 @@ pipeline {
                     def dockerImage = docker.build("cruelplatypus67/devops2:${env.BUILD_ID}", "-f Dockerfile .")
                     env.IMAGE_NAME = "cruelplatypus67/devops2:${env.BUILD_ID}"
                     sh "docker image push cruelplatypus67/devops2:${env.BUILD_ID}"
+                    sh "docker tag cruelplatypus67/devops2:${env.BUILD_ID} cruelplatypus67/devops2:latest"
+                    sh "docker image push cruelplatypus67/devops2:latest"
                 }
             }
         }
