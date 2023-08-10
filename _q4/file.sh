6a961e76cda3a2125421d5ce6dfafb4cc1f50cea
Suraj Mandal <surajmandalcell@gmail.com>
2023-08-10 09:09:35 -0400

    Update final

--- _q4/file.sh
+++ _q4/file.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 TO_EMAIL="surajmandalcell@gmail.com"
-
 SUBJECT="Pending Pods Found!"
 
 PENDING_PODS=$(kubectl get pods --field-selector=status.phase=Pending -o json | jq -r '.items[].metadata.name')
 TIMESTAMP=$(date +"%Y-%m-%d %H-%M-%S")
-
 
 if [ -n "$PENDING_PODS" ] && [ -n "$TIMESTAMP" ]; then
     echo "$PENDING_PODS - $TIMESTAMP" | ssmtp surajmandalcell@gmail.com
